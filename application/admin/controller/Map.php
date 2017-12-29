be0d6e97398ff5484dcb97e92d0fa415263f918f
a6665119d <79682884@qq.com>
2017-12-29 18:19:39 +0800

    新增查看简陋考勤表

--- application/admin/controller/Map.php
+++ application/admin/controller/Map.php
@@ -6,6 +6,19 @@ class Map extends \think\Controller
     public function index()
     {
         return $this->fetch();
-    } 
+        
+    }
+    public function sign(){
+    	$signlist=db('sign')->select();
+    	$this->assign("signlist",$signlist);
+    	return $this->fetch();
+    }
+    public function addmap(){
+        $addData = input();
+        print_r($addData);
+    	$addData['time'] = time();
+    	$addData['uid'] = 1;
+    	db('sign')->insert($addData);
+    }
 }
 
