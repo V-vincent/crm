5b87ce1d0e4b94c86208924c1bdc54018f8a20ba
hexuan123 <1127363751@qq.com>
2018-01-04 10:09:18 +0800

    crm视图编辑页面完成

--- application/admin/controller/Crm.php
+++ application/admin/controller/Crm.php
@@ -7,7 +7,7 @@ class Crm extends \think\Controller
     {
          //查询公司分类
         $companycate_list = db('companycate')->select();
-        $this->assign('companycate_list',$companycate_list);
+        $this->assign('companycate_list',json_encode($companycate_list));
         $where_data=[];
         //如果有传搜索条件
         //那么就加一个链式操作
@@ -43,6 +43,66 @@ class Crm extends \think\Controller
          //编辑查询一条信息,编辑
         $id = input('id');
         $info = db('userinfo')->where("id=$id")->find();
+        $this->assign('info',$info);
         return $info;
     }
+    //客户来源数据词典
+    public function usersource(){
+        $list = db('usersource')->paginate(10);
+        $this->assign('list',$list);
+        return $this->fetch();
+    }
+       //更改客户来源数据词典
+    public function update_usersource(){
+        foreach ($_REQUEST['user_source'] as $key => $value) {
+            db('usersource')->where("id=$key")->update(['user_source'=>$value]);
+        }
+     $this->success('','usersource');
+       
+ }
+ //客户类型信息
+ public function companycate(){
+      $list = db('companycate')->paginate(10);
+     $this->assign('list',$list);
+     return $this->fetch();
+ }
+ //更改客户类型
+  public function update_companycate(){
+        foreach ($_REQUEST['companycate'] as $key => $value) {
+            db('companycate')->where("id=$key")->update(['company_cate'=>$value]);
+        }
+     $this->success('','companycate');
+       
+ }
+
+ //查看每个用户的信息
+ public function userinfo(){
+  // $id=input('id');
+  // $info = db('userinfo')->where("id=$id")->find();
+       $id=input('id'); 
+       //查询公司类型
+       $companycate_list = db('companycate')->select();
+        $this->assign('companycate_list',$companycate_list);
+        $where_data=[];
+        if(input('companycate_id')>0){
+            $where_data['companycate_id'] = input('companycate_id');
+        }
+       //查询客户来源
+        $usersource_list = db('usersource')->select();
+        $this->assign('usersource_list',$usersource_list);
+        $where_data2=[];
+        if(input('usersource_id')>0){
+            $where_data['usersource_id'] = input('usersource_id');
+        }
+        //查询表
+       $info = db('userinfo')->alias('u')
+                        ->join('companycate c','u.companycate_id=c.id','left')
+                        ->join('usersource s','u.usersource_id=s.id','left')
+                        ->where($where_data)
+                        ->where($where_data2)
+                        ->where("u.id=$id")
+                        ->find();
+  $this->assign('info',$info);
+  return $this->fetch();
+ }
 }
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,11 +7,12 @@ class Index extends \think\Controller
     {
     	$list = db('companycate')->select();
     	$this->assign('list',$list);
+        $user_source = db('usersource')->select();
+        $this->assign('user_source',$user_source);
         return $this->fetch();
     }
     //添加客户
-    public function save(){
-        
+    public function save(){       
         $time=date('y-m-d');
         $time="20".$time;
         $data=input();
