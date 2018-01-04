7b0bcf97570206fac55317689c9a1af2d48985e6
zhangzongdong <2663516753@qq.com>
2018-01-04 12:13:22 +0800

    build

--- application/admin/controller/Build.php
+++ application/admin/controller/Build.php
@@ -3,6 +3,15 @@ namespace app\admin\controller;
 
 class Build extends \think\Controller
 {
+    public function index(){
+        return $this->fetch();
+    }
+    public function add(){
+        return $this->fetch();
+    }
 
+    public function edit(){
+        return $this->fetch();
+    }
 }
 ?>
