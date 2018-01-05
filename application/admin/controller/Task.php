0d1c5eb6bc870217efb367e314139d181db85232
a6665119d <79682884@qq.com>
2018-01-05 15:17:30 +0800

    解决冲突

--- application/admin/controller/Task.php
+++ application/admin/controller/Task.php
@@ -13,6 +13,17 @@ class Task extends \think\Controller
 
     }
     public function look(){
+    	   //如果没有传入年份则获取当前系统年份
+     $year=input('y')>0?input('y'):date('Y');
+    //如果没有传入月份则获取当前系统月份
+    $month=input('m')>0?input('m'):date('m');
+
+    	$this->assign('year',$year);
+    	$this->assign('month',intval($month));
+
+    //获取当前月有多少天
+    $days=date('t',strtotime("{$year}-{$month}-1"));
+    	$this->assign('t_days',$days);
     	return $this->fetch();
     }      
 
