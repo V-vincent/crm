1b5f86522557b3ef26a275a478695e462a492f4f
pengjunqiang <1322388862@qq.comgit gitgit commit -m git config --global user.name Your>
2018-01-06 21:41:49 +0800

    task.php

--- application/admin/controller/Task.php
+++ application/admin/controller/Task.php
@@ -7,8 +7,10 @@ class Task extends \think\Controller
     {
         return $this->fetch();
     }    
-	public function write()
+	public function add()
     {
+        $time=date('Y-m-d');
+        $this->assign("time",$time);
         return $this->fetch();
     }
     public function check()
@@ -22,7 +24,24 @@ class Task extends \think\Controller
 	public function gather()
     {
         return $this->fetch();
-    }       
+    }  
+    public function save()
+    {
+        $data=input();
+        $time=date('Y-m-d H:i:s');
+        $data['time'] = $time;        
+        $work = request()->file('work');
+        if($work){
+            $info = $work->move(ROOT_PATH.'/public/uploads');
+            if($info){
+                $data['work'] = $info->getSaveName();
+            }else{
+                 echo $info->getError();
+            }
+        }
+        db("work")->insert($data);
+        $this->success('添加成功','index');
+    }     
 
 }
 
