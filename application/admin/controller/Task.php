02751fd1ab2d113c921d9a21fad9747db718fcbc
pengjunqiang <1322388862@qq.comgit gitgit commit -m git config --global user.name Your>
2018-01-07 20:14:58 +0800

    作业管理动态数据

--- application/admin/controller/Task.php
+++ application/admin/controller/Task.php
@@ -5,6 +5,8 @@ class Task extends \think\Controller
 {
     public function index()
     {
+        $work_list  =  db('work')->order('id desc')->paginate(5);
+        $this->assign('work_list',$work_list);                   
         return $this->fetch();
     }    
 	public function add()
@@ -13,28 +15,6 @@ class Task extends \think\Controller
         $this->assign("time",$time);
         return $this->fetch();
     }
-
-    public function look(){
-    	   //如果没有传入年份则获取当前系统年份
-     	$year=input('y')>0?input('y'):date('Y');
-    	//如果没有传入月份则获取当前系统月份
-    	$month=input('m')>0?input('m'):date('m');
-    	$this->assign('year',$year);
-    	$this->assign('month',intval($month));
-    	//获取当前月有多少天
-   		 $days=date('t',strtotime("{$year}-{$month}-1"));
-    		$this->assign('t_days',$days);
- 
-    		return $this->fetch();
-    }
-    public function check()
-    {
-        return $this->fetch();
-    }          
-	public function gather()
-    {
-        return $this->fetch();
-    }  
     public function save()
     {
         $data=input();
@@ -52,6 +32,36 @@ class Task extends \think\Controller
         db("work")->insert($data);
         $this->success('添加成功','index');
     }     
-
+    public function look(){
+    	   //如果没有传入年份则获取当前系统年份
+     	$year=input('y')>0?input('y'):date('Y');
+    	//如果没有传入月份则获取当前系统月份
+    	$month=input('m')>0?input('m'):date('m');
+    	$this->assign('year',$year);
+    	$this->assign('month',intval($month));
+    	//获取当前月有多少天
+   		 $days=date('t',strtotime("{$year}-{$month}-1"));
+    		$this->assign('t_days',$days);
+    		return $this->fetch();
+    }
+    public function check()
+    {
+        $id = input('id');
+        $check = db('work')->where("id=$id")->select();
+        $this->assign('check',$check);           
+        return $this->fetch();
+    }          
+	public function gather()
+    {
+        return $this->fetch();
+    }  
+   
+    public function update()
+    {
+         $id = input('id');
+         $data=input();
+        db('work')->where("id=$id")->update($data);
+        $this->success('编辑成功','index');      
+    } 
 }
 
